fbcf5827ff79c64dc711b9ce837e1dc723877a92
Quid15k <arushpc.15k@gmail.com>
2025-08-31 18:33:25 +0000

    test arrays

--- arrays/ArrayBasics.java
+++ arrays/ArrayBasics.java
@@ -3,5 +3,9 @@ import java.util.*;
 public class ArrayBasics{
     public static void main(String[] args){
         System.out.println("hello, arrays!");
+	int[] arr = {1,2,3,4};
+	for(int i : arr){
+ 	    System.out.println(i);
+	}
     }	
 }
